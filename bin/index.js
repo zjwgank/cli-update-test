3686395eb1649e665de7623edbfc6e0c7b402ddd
zjwgank <zjwgank@appledeMacBook-Pro.local>
2020-03-17 17:14:11 +0800

    feat: cli init

---
+++ bin/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+// const readline = require('readline')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const program = require('commander')
+
+const { IsGit } = require('../lib')
+
+// const rl = readline.createInterface(process.stdin, process.stdout)
+
+// rl.question('what is your name?', res => {
+//   console.log(`name is ${res}`)
+//   rl.close()
+// })
+
+const tips = chalk.yellow(figlet.textSync('cliDemo', { font: 'Ghost', horizontalLayout: 'default', verticalLayout: 'default' }))
+console.log(tips)
+
+program.version('0.0.1')
+program.name('cli')
+
+program.usage('[ options ] command')
+
+program.command('update').description('update cli demo').action(() => {
+  console.log('update----------')
+})
+
+program.parse(process.argv)
+
+if (!program.args.length) {
+  program.help()
+}
+
+if (!IsGit(".git")) {
+  console.log(chalk.red('It`s sorry for no git'))
+  process.exit()
+}
+
+
